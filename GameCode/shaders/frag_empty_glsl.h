0eee327515a2f7d0724e319ed6c843834e409d8d
ercan <xercax@gmail.com>
2024-11-21 21:24:12 +0300

    Improved the embed_shaders script and cmake

--- GameCode/shaders/frag_empty_glsl.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-const char * frag_empty_source =
-"#version 330 core\n"
-"\n"
-"void main() {}\n"
-;
--- embed_shaders.bat
+++ embed_shaders.bat
@@ -1,16 +1,23 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:: Use the first argument as the configuration
+set CONFIG=%~1
+
 set INPUT_DIR=GameCode
 set OUTPUT_DIR=GameCode\shaders
 set FILE_FOUND=0
+
+if not exist "%OUTPUT_DIR%" (
+    mkdir "%OUTPUT_DIR%"
+)
 
 for %%F in (%INPUT_DIR%\*.glsl) do (
     set FILE_NAME=%%~nF
     set SOURCE_TYPE=!FILE_NAME!_source
     set OUTPUT_FILE=!OUTPUT_DIR!\!FILE_NAME!_glsl.h
 
-    build\ShaderEmbedder\Release\ShaderEmbedder.exe !SOURCE_TYPE! "%%F" "!OUTPUT_FILE!"
+    build\ShaderEmbedder\%CONFIG%\ShaderEmbedder.exe !SOURCE_TYPE! "%%F" "!OUTPUT_FILE!"
 
     set /a FILE_FOUND+=1
 )
